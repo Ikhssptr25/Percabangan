8c8570f3c942a91a2525b2be94e7637c86827485
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-11-13 16:45:59 +0700

    Add files via upload

---
+++ latihan2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+   string nama;
+   int jumlah;
+   int lama;
+   int bunga;
+   
+   cout<<"masukan nama   : "; 
+   getline(cin, nama);
+   cout<<"masukan jumlah : ";
+   cin>>jumlah;
+   cout<<"masukan lama   : ";
+   cin>>lama;
+   cout<<"masukan bunga  :";
+   cin>>bunga;
+   
+   cout<<endl;
+   cout<<"nama   :"<<nama <<endl;
+   cout<<"jumlah :"<<jumlah<<endl;
+   cout<<"lama   :"<<lama;
+   cout<<" tahun "<<endl;
+   cout<<"bunga  :"<<bunga;
+   cout<<"%"<<endl;
+   return 0;
+
+}
