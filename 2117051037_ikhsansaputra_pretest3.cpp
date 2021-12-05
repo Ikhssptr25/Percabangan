63af96fa2a106e59d6721e5b24a40deae0bd00c6
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-05 19:50:53 +0700

    Add files via upload

---
+++ 2117051037_ikhsansaputra_pretest3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	
+	// deklarasi variabel//
+	int gaji_karyawan;
+	int UMR_daerah ;
+	
+	//input data
+	cout<<"GAJI KARYAWAN :"; 
+	cin>>gaji_karyawan;
+	cout<<"UMR DAERAH    :";
+	cin>>UMR_daerah;
+	cout<<endl;
+	
+	// percabangan output
+	cout<<"KETERANGAN GAJI "<<endl;
+	if(gaji_karyawan<UMR_daerah){
+	cout<<"GAJI ANDA DIBAWAH UMR DAERAH"<<endl;
+	} else if (gaji_karyawan==UMR_daerah){
+	cout<<"GAJI ANDA SETARA UMR DAERAH"<<endl;
+	} else {
+	cout<<"GAJI ANDA DIATAS UMR DAERAH"<<endl;
+	}
+	
+	
+	return 0;
+	
+	
+}
