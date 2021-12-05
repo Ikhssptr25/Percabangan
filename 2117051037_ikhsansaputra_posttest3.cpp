fd4f3ba90586aa4650b1852807c41653ec47e5de
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-05 19:50:37 +0700

    Add files via upload

---
+++ 2117051037_ikhsansaputra_posttest3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	
+	// deklarasi variabel//
+	double BTC;
+	
+	
+	//input data
+	cout<<"JUMLAH BITCOIN :";  
+	cin>>BTC;
+	cout<<endl;
+	
+	// percabangan output
+	cout<<" --KETERANGAN BITCOIN-- "<<endl;
+	cout<<endl;
+	
+	if(BTC<1){
+	cout<<"Tipe Ranking : Shrimp";
+	} else if (BTC>=1 && BTC<10){
+	cout<<"Tipe Ranking : Crab" ;
+	} else if (BTC>=10 && BTC<50){
+	cout<<"Tipe Ranking : Octopus";
+	} else if (BTC>=50 && BTC<100){
+	cout<<"Tipe Ranking : Fish";	
+	} else if (BTC>=100 && BTC<500){
+	cout<<"Tipe Ranking : Dolphin";
+	} else if (BTC>=500 && BTC<1000){
+	cout<<"Tipe Ranking : Shark";
+	} else if (BTC>=1000 && BTC<5000){
+	cout<<"Tipe Ranking : Whale";
+	}else{ 
+	cout<<"Tipe Ranking : Humpback"<<endl;
+	}
+	
+	
+	
+	return 0;
+}
