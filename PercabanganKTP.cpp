c99a288742c472306d2303703a69709a7a942876
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-05 19:51:23 +0700

    Add files via upload

---
+++ PercabanganKTP.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	
+	// deklarasi variabel//
+	int ktp;
+	 
+	
+	//input data
+	cin>>ktp;
+	cout<<endl;
+	
+	// percabangan output
+	cout<<"KETERANGAN"<<endl;
+	if(ktp<17){
+	cout<<"umur anda belum mencukupi dalam pembuatan ktp"<<endl;
+	} else {
+	cout<<"umur anda sudah mencukupi dalam pembuatan ktp"<<endl;
+	}
+	
+	
+	return 0;
+	
+	
+}
