337ee6e0a7749302ce2f524b85e996d61129237d
Ikhsan saputra <92314859+Ikhssptr25@users.noreply.github.com>
2021-12-05 19:51:46 +0700

    Add files via upload

---
+++ PercabanganNilai.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	
+	/*ikhsan saputra
+	  2117051037
+	  kelas B*/
+	  
+   	
+	// deklarasi variabel//
+	int nilai;
+	 
+	
+	//input data
+	cout<<"Nilai sekolah:"; 
+	cin>>nilai;
+	cout<<endl;
+	
+	// percabangan output
+	cout<<"KETERANGAN NILAI "<<endl;
+	if(nilai<75){
+	cout<<"Nilai anda belom mencukupi KKM"<<endl;
+	} else {
+	cout<<"Nilai anda sudah mencukupi KKM"<<endl;
+	}
+	
+	
+	return 0;
+	
+	
+}
